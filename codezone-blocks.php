7dfcfd2071776f646c12276fdd424243dcdb5f92
Craig Wann <craig@codezone.io>
2022-03-11 10:46:30 -0600

    Versioning

--- codezone-blocks.php
+++ codezone-blocks.php
@@ -7,7 +7,7 @@
  * Author URI:      https://codezone.io
  * Text Domain:     codezone-blocks
  * Domain Path:     /languages
- * Version:         1.4.0
+ * Version:         1.6.0
  * GitHub Plugin URI: thecodezone/codezone-blocks
  *
  * @package         Codezone_Blocks
