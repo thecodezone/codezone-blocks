6f21be2f278e6d586e4fccbfcbcaeaaa8f455a1e
Craig Wann <craig@codezone.io>
2022-04-13 15:27:05 -0500

    Fixing vertical accordion mobile width bug.

--- codezone-blocks.php
+++ codezone-blocks.php
@@ -7,7 +7,7 @@
  * Author URI:      https://codezone.io
  * Text Domain:     codezone-blocks
  * Domain Path:     /languages
- * Version:         1.8.6
+ * Version:         1.8.7
  * GitHub Plugin URI: thecodezone/codezone-blocks
  *
  * @package         Codezone_Blocks
