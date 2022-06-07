387d007d984b609f1b6966742ab20040933580d7
Craig Wann <craig@codezone.io>
2022-06-07 18:22:49 -0500

    Making swiper key generic.

--- blocks/carousel/register.php
+++ blocks/carousel/register.php
@@ -7,8 +7,8 @@ acf_register_block_type([
     'render_callback' => cz_render_acf_block('cz-carousel', __dir__ . '/block.php'),
     'icon' => '<svg aria-hidden="true" style="color: #EE4538" focusable="false" data-prefix="fad" data-icon="presentation" class="svg-inline--fa fa-presentation fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><g class="fa-group"><path class="fa-secondary" fill="currentColor" d="M480 64h64v256a32 32 0 0 1-32 32H64a32 32 0 0 1-32-32V64h64v224h384z" opacity="0.4"></path><path class="fa-primary" fill="currentColor" d="M560 0H16A16 16 0 0 0 0 16v32a16 16 0 0 0 16 16h544a16 16 0 0 0 16-16V16a16 16 0 0 0-16-16zM320 386.75V352h-64v34.75l-75.31 75.31a16 16 0 0 0 0 22.63l22.62 22.62a16 16 0 0 0 22.63 0L288 445.25l62.06 62.06a16 16 0 0 0 22.63 0l22.62-22.62a16 16 0 0 0 0-22.63z"></path></g></svg>',
     'enqueue_assets' => function(){
-        wp_enqueue_script( 'cz-swiper-js', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/7.4.1/swiper-bundle.min.js', true);
-        wp_enqueue_style( 'cz-swiper-css', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/7.4.1/swiper-bundle.css');
+        wp_enqueue_script( 'swiper', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/7.4.1/swiper-bundle.min.js', true);
+        wp_enqueue_style( 'swiper', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/7.4.1/swiper-bundle.css');
         wp_enqueue_style( 'cz-carousel', CZ_BLOCKS_URI . 'blocks/carousel/style.css', [], CZ_BLOCKS_VERSION);
         wp_enqueue_script( 'cz-carousel', CZ_BLOCKS_URI . 'blocks/carousel/script.js', ['cz-swiper-js'], CZ_BLOCKS_VERSION, true );
     },
--- blocks/image-carousel/register.php
+++ blocks/image-carousel/register.php
@@ -5,8 +5,8 @@ acf_register_block_type( [
     'description'     => 'An image carousel.',
     'render_callback' => cz_render_acf_block( 'cz-image-carousel', __dir__ . '/block.php' ),
     'enqueue_assets'  => function () {
-        wp_enqueue_script( 'cz-swiper-js', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/7.4.1/swiper-bundle.min.js', true );
-        wp_enqueue_style( 'cz-swiper-css', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/7.4.1/swiper-bundle.css' );
+        wp_enqueue_script( 'swiper', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/7.4.1/swiper-bundle.min.js', true );
+        wp_enqueue_style( 'swiper', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/7.4.1/swiper-bundle.css' );
         wp_enqueue_style( 'cz-image-carousel', CZ_BLOCKS_URI . 'blocks/image-carousel/style.css', [], CZ_BLOCKS_VERSION );
         wp_enqueue_script( 'cz-image-carousel', CZ_BLOCKS_URI . 'blocks/image-carousel/script.js', [ 'cz-swiper-js' ], CZ_BLOCKS_VERSION, true );
     },
